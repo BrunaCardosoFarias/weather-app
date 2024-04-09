7e12cf61d76734e239a41a9b4d30fcdaab3b45ca
Bruna Farias <brunacardosodefarias@gmail.com>
2024-04-09 16:13:19 -0300

    The application is now fully functional

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Weather from './Weather.js';
 import './style.css';
 const App = () => {
   return (
-    <>      
-    <h1>Weather Forecast App</h1>
+    <>       
      <div>
       <Weather />
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -32,30 +32,45 @@ const Weather = () => {
     e.preventDefault();
     fetchData();
   };
+  const getBackgroundStyle = () => {
+    if (weatherData && weatherData.main.temp > 25) {
+      return {  
+        backgroundColor: '#FFA500',
+        transition: 'background-color 1s ease'
+      };
+    } else {
+      return {     
+        backgroundColor: '#B0C4DE',
+        transition: 'background-color 1s ease'
+      };
+    }
+  };
 
   return (
-    <div>
+    <div style={getBackgroundStyle()}> 
+      <h1>Weather App</h1>
       <form onSubmit={handleSubmit} className="input">
-        <input        
+        <input        className="input-city"
           type="text"
           placeholder="Enter city name"
           value={city}
           onChange={handleInputChange}
         />
-        <button type="submit">Get Weather</button>
+        <button type="submit" className="button">Get Weather</button>
       </form>
-      {weatherData &&(
+      {weatherData &&(        
         <>
-          <h2>{weatherData.name}</h2>
+        <h2>{weatherData.name}</h2>
           <p>Temperature: {weatherData.main.temp}°C</p>
           <p>Description: {weatherData.weather[0].description}</p>
           <p>Feels like : {weatherData.main.feels_like}°C</p>
           <p>Humidity : {weatherData.main.humidity}%</p>
           <p>Pressure : {weatherData.main.pressure}</p>
           <p>Wind Speed : {weatherData.wind.speed}m/s</p>
-        </>
+          </>  
+    
       )}
-    </div>
+    </div>   
   );
 };
 
